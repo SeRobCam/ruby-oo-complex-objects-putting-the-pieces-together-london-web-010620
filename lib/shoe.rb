c1e55227630ca1977f40c4b160a0d2023249584e
Sergio Robles <sroblesc@gmail.com>
2020-01-08 09:50:09 +0000

    Automatically backed up by Learn

--- lib/shoe.rb
+++ lib/shoe.rb
@@ -2,7 +2,7 @@
 
 class Shoe 
   
-  attr_accessor :brand, :color, :size, :material, :condition = "new"
+  attr_accessor :brand, :color, :size, :material, :condition
   
   def initialize(brand)
     @brand = brand 
